2a46551421abdc28c4be16d25dff079471a350d3
qvedroid <qvedroid@gmail.com>
2018-01-20 23:57:30 +0800

    img_to_amp-img.js
    Fungsi untuk mengganti pada TAG img ke amp-img untuk amp html blogger

---
+++ amp/img_to_amp-img.js
@@ -0,0 +1,3 @@
+<script>
+$('.thumbnail').find('img').replaceWith(function () { return '<amp-img src="'+this.src+'" width="1080" height="610" layout="responsive" alt="'+this.alt+'"></amp-img>' });
+</script>
