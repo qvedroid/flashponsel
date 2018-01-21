5ab3d5d442483d115c18daec66fa1566b213e115
qvedroid <qvedroid@gmail.com>
2018-01-21 12:49:13 +0800

    g_cse.js
    search engine for blogger

---
+++ amp/g_cse.js
@@ -0,0 +1,9 @@
+ (function() {
+    var cx = '005345254758898473466:r9indx-jdve';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
